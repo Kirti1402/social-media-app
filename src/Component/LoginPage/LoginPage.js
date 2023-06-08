3a6b31c601d9f2cef2a3bef091226a32d118ae0a
Kirti Singh <kirtisingh1497@gmail.com>
2023-06-08 20:09:31 +0530

    style : created main banner

--- src/Component/LoginPage/LoginPage.js
+++ src/Component/LoginPage/LoginPage.js
@@ -3,8 +3,8 @@ import { authContext } from "../../Context/AuthContext";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import image from "../../Assets/AuthPageBanner.png"
-import "./Login.css"
+import "./Login.css";
+import MainBanner from "./MainBanner";
 
 export default function LoginPage() {
   const { authState, authDispatch, login, inputValidation } =
@@ -19,61 +19,49 @@ export default function LoginPage() {
     authDispatch({ type: "SET_PASSWORD", payload: "aspoghmz." });
   };
   return (
-   <div className="login-conatiner">
-    <div className="text-container">
-        {/* <img src={image}/> */}
-        <div className="text-detail">
-        <h1 className="title">
-            <span>Explore</span>
-            <span>Together</span>
-            </h1>
-        <h4 >FOLLOW<span className="span-text">PEOPLE AROUND THE GLOBE</span></h4>
-        <h4>CONNECT<span className="span-text">WITH YOUR TRAVELLERS</span></h4>
-        <h4>SHARE<span className="span-text">YOUR EXPERIENCE</span></h4>
-        </div>
-        
-        </div>
-     <div className="login-form-container">
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="outlined-basic"
-          label="UserName"
-          size="small"
-          variant="outlined"
-          value={authState.username}
-          onChange={(e) =>
-            authDispatch({ type: "SET_USERNAME", payload: e.target.value })
-          }
-        />
-        <TextField
-          id="outlined-basic"
-          size="small"
-          label="Password"
-          variant="outlined"
-          value={authState.password}
-          onChange={(e) =>
-            authDispatch({ type: "SET_PASSWORD", payload: e.target.value })
-          }
-        />
+    <div className="login-conatiner">
+      <MainBanner/>
+      <div className="login-form-container">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="UserName"
+            size="small"
+            variant="outlined"
+            value={authState.username}
+            onChange={(e) =>
+              authDispatch({ type: "SET_USERNAME", payload: e.target.value })
+            }
+          />
+          <TextField
+            id="outlined-basic"
+            size="small"
+            label="Password"
+            variant="outlined"
+            value={authState.password}
+            onChange={(e) =>
+              authDispatch({ type: "SET_PASSWORD", payload: e.target.value })
+            }
+          />
+          <div>
+            <Button variant="contained" size="small" color="primary">
+              Log In
+            </Button>
+          </div>
+        </Box>
         <div>
-          <Button variant="contained" size="small" color="primary">
-            Log In
+          <Button size="small" color="primary" onClick={handleGuestLogin}>
+            Guest Login
           </Button>
         </div>
-      </Box>
-      <div>
-        <Button size="small" color="primary" onClick={handleGuestLogin}>
-          Guest Login
-        </Button>
       </div>
-      </div>
-   </div>
+    </div>
   );
 }
---
+++ src/Component/LoginPage/MainBanner.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function MainBanner() {
+  return (
+    <div className="text-container">
+        {/* <img src={image}/> */}
+        <div className="text-detail">
+          <h1 className="title">
+            <span>Explore</span>
+            <span>Together</span>
+          </h1>
+          <div className="sub-title">FOLLOW</div>
+          <div className="sub-title-1">
+            <span>PEOPLE AROUND THE GLOBE</span>
+          </div>
+
+          <div className="sub-title"> CONNECT</div>
+          <div className="sub-title-1">
+            <span>WITH YOUR TRAVELLERS</span>
+          </div>
+
+          <div className="sub-title"> SHARE</div>
+          <div className="sub-title-1">
+            <span>YOUR EXPERIENCE</span>
+          </div>
+        </div>
+      </div>
+  )
+}
