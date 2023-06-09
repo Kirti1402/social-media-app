cb4b672a7a95e548f245d2cf66a4ce5e5f8469fa
Kirti Singh <kirtisingh1497@gmail.com>
2023-06-09 23:17:23 +0530

    feat : profile page component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import LoginPage from './Component/LoginPage/LoginPage';
-import {Routes,Route,Link} from "react-router-dom"
-import HomePage from './Component/HomePage/HomePage';
-import LandingPage from './Component/HomePage/LandingPage';
-import { ProtectedRoutes } from './ProtectedRoutes';
-import SignUpPage from './Component/SignupPage/SignUpPage';
-
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoginPage from "./Component/LoginPage/LoginPage";
+import { Routes, Route, Link } from "react-router-dom";
+import HomePage from "./Component/HomePage/HomePage";
+import LandingPage from "./Component/HomePage/LandingPage";
+import { ProtectedRoutes } from "./ProtectedRoutes";
+import SignUpPage from "./Component/SignupPage/SignUpPage";
+import Profile from "./Component/ProfilePage/Profile";
 
 function App() {
   return (
     <div className="App">
       <ToastContainer />
-          <Routes>
-            <Route path='/login' element={<LoginPage />}/>
-            <Route path='/' element={<HomePage />}/>
-            <Route path='/signup' element={<SignUpPage/>} />
-            {/* <Route path='/login' element={<LoginPage />}/> */}
-            <Route
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        {/* <Route path='/login' element={<LoginPage />}/> */}
+        <Route
           path="/landing"
           element={
             <ProtectedRoutes>
@@ -26,7 +26,16 @@ function App() {
             </ProtectedRoutes>
           }
         />
-          </Routes>
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoutes>
+              <Profile />
+            </ProtectedRoutes>
+          }
+        />
+      </Routes>
     </div>
   );
 }
