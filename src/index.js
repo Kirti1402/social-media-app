3b9d4d432e3bcc9998519b865fd73e10af4c2163
Kirti Singh <kirtisingh1497@gmail.com>
2023-06-15 13:01:30 +0530

    feat: added post cntext

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { AuthProvider } from "./Context/AuthContext";
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
 import { UserProvider } from "./Context/allUser";
 import { FollowUnfollowProvider } from "./Context/FollowUnFollowContext";
+import { PostProvider } from "./Context/PostContext";
+
 // Call make Server
 makeServer();
 
@@ -14,13 +16,15 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <AuthProvider>
-      <UserProvider>
-      <FollowUnfollowProvider> 
-      <App />
-      </FollowUnfollowProvider> 
-      </UserProvider>
-    </AuthProvider>
+      <AuthProvider>
+        <UserProvider>
+          <FollowUnfollowProvider>
+            <PostProvider>
+              <App />
+            </PostProvider>
+          </FollowUnfollowProvider>
+        </UserProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
